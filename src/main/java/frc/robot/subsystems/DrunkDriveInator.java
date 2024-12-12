d98f50bea4f88a1b384929f247e9f55853133b58
Ironclad33 <64609905+Ironclad33@users.noreply.github.com>
2024-12-12 11:30:30 -0600

    Update DrunkDriveInator.java

--- src/main/java/frc/robot/subsystems/DrunkDriveInator.java
+++ src/main/java/frc/robot/subsystems/DrunkDriveInator.java
@@ -1,7 +1,7 @@
-package frc.robot.commands;
+package frc.robot.subsystems;
 
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DrunkDriveInator{
   public DrunkDriveInator(){
