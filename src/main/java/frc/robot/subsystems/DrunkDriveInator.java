88a0ad5d579e2c0e8a9694e4d5f4426f69ce3c6f
Ironclad33 <64609905+Ironclad33@users.noreply.github.com>
2024-12-12 13:15:18 -0600

    Update DrunkDriveInator.java

--- src/main/java/frc/robot/subsystems/DrunkDriveInator.java
+++ src/main/java/frc/robot/subsystems/DrunkDriveInator.java
@@ -3,8 +3,16 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import frc.robot.subsystems.swerve.DriveSubsystem;
+import frc.robot.subsystems.swerve.SwerveModule;
+import frc.robot.Constants;
+
 public class DrunkDriveInator{
-  public DrunkDriveInator(){
+  public void getDDISpeed(){
+    int ddiCoef = Math.nextInt(Constants.ddiMaxVelocity);
+    double ddiSpeed = Math.random()*xcoef;
+    
+    return ddiSpeed;
     
   }
 }
