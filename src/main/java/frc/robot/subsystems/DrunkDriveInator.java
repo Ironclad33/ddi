2a1cce9cc4035a8644d5a8ecb706b2726bbf40a5
Ironclad33 <64609905+Ironclad33@users.noreply.github.com>
2024-12-12 11:29:22 -0600

    Create DrunkDriveInator.java

---
+++ src/main/java/frc/robot/subsystems/DrunkDriveInator.java
@@ -0,0 +1,10 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class DrunkDriveInator{
+  public DrunkDriveInator(){
+    
+  }
+}
