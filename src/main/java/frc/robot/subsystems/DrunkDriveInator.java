4e410e9ada70c2341d54dbe7658e1972602479ac
Ironclad33 <64609905+Ironclad33@users.noreply.github.com>
2024-12-12 20:38:12 -0600

    Update DrunkDriveInator.java

--- src/main/java/frc/robot/subsystems/DrunkDriveInator.java
+++ src/main/java/frc/robot/subsystems/DrunkDriveInator.java
@@ -3,15 +3,23 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import java.util.concurrent.ThreadLocalRandom;
+
+
 import frc.robot.subsystems.swerve.DriveSubsystem;
 import frc.robot.subsystems.swerve.SwerveModule;
 import frc.robot.Constants;
 
 public class DrunkDriveInator{
-  public void getDDISpeed(){
-    int ddiCoef = Math.nextInt(Constants.ddiMaxVelocity);
-    double ddiSpeed = Math.random()*xcoef;
+  public double getDDISpeed(){
+    //gets a number to use for calling xspeed and yspeed drive in the DrunkDriveInator
     
+
+    int ddiCoef = ThreadLocalRandom.current().nextInt(-Constants.ddiMaxVelocity,Constants.ddiMaxVelocity);
+    System.out.println(ddiCoef);
+    double ddiSpeed = Math.random()*ddiCoef;
+    System.out.println("ddi speed");
+    System.out.println(ddiSpeed);
     return ddiSpeed;
     
   }
